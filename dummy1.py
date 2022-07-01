4b73b7b6925cca58644ed01398f5407a63175226
Ibrahim Koc <iibrahimkoc@gmail.com>
2022-07-02 01:38:47 +0300

    model ckpt

--- dummy1.py
+++ dummy1.py
@@ -1,3 +1,4 @@
+import enum
 import torch
 from torchvision import models
 import torch.nn as nn
@@ -13,6 +14,16 @@ if debugMode:
     layer6 = list(layers[6].children())
     layer7 = list(layers[7].children())
 
+class subnet:
+    def __init__(self,layers):    
+        self.lastStage = nn.Sequential(*layers[0])
+        self.fullyConnected = nn.Sequential(*layers[1:]) 
+
+    def forward(self,x):
+        x = self.lastStage(x)
+        x = self.fullyConnected(x)
+        return x
+
 class model1:
     def __init__(self):    
         super(self).__init__()
@@ -20,22 +31,15 @@ class model1:
         # .children(), .modules() difference: https://discuss.pytorch.org/t/module-children-vs-module-modules/4551/3
         layers = list(resnet50.children())
         self.backbone = nn.Sequential(*layers[:6])
-        self.f1 = nn.Sequential(*layers[7])
-        self.g1 = nn.Sequential(*layers[7])
-        self.f2 = nn.Sequential(*layers[8:])
-        self.g2 = nn.Sequential(*layers[8:])
-    def forward(self, x, mode):
+        self.subnetU = subnet(layers[7:])
+        self.subnetR = subnet(layers[7:])
+
+    def forward(self, x):
         x = self.backbone(x)
-        if mode == "u":
-            x = self.f1(x)
-            x = self.f2(x)
-        elif mode == "r":
-            x = self.g1(x)
-            x = self.g2(x)
-        elif mode == "ur":
+        u = self.subnetU.forward(x)
+        r = self.subnetR.forward(x)
+        return u,r    
             
-
-
 
 print("dummy1_1")
 print("dummy1_2")
